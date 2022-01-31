dd64a0960a8bf4c1fa1578b14d7ef8e18e17a234
olzhs <oleg1984@mail.ru>
2022-01-31 22:30:08 +0500

    filling the list with test data

--- To-Do Manager/TaskListController.swift
+++ To-Do Manager/TaskListController.swift
@@ -9,38 +9,110 @@ import UIKit
 
 class TaskListController: UITableViewController {
 
+    var tasksStorage: TasksStorageProtocol = TaskStorage()
+    var tasks: [TaskPriority:[TaskProtocol]] = [:]
+    var sectionsTypesPosition: [TaskPriority] = [.impotant, .normal]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadTasks()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    private func loadTasks() {
+        sectionsTypesPosition.forEach { taskType in
+            tasks[taskType] = []
+        }
+        tasksStorage.loadTasks().forEach { task in
+            tasks[task.type]?.append(task)
+        }
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return tasks.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        let taskType = sectionsTypesPosition[section]
+        
+        guard let currentTasksType = tasks[taskType] else {
+            return 0
+        }
+        
+        return currentTasksType.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
 
         // Configure the cell...
 
+        return getConfiguredTaskCell_constraints(for: indexPath)
+    }
+    
+    
+    private func getConfiguredTaskCell_constraints(for indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        let taskType = sectionsTypesPosition[indexPath.section]
+        
+        guard let currentTask = tasks[taskType]?[indexPath.row] else {
+            return cell
+        }
+        
+        let symbolLabel = cell.viewWithTag(1) as? UILabel
+        let textLabel = cell.viewWithTag(2) as? UILabel
+        
+        symbolLabel?.text = getSymbolForTask(with: currentTask.status)
+        textLabel?.text = currentTask.title
+        
+        if currentTask.status == .planned {
+            textLabel?.textColor = .black
+            symbolLabel?.textColor = .black
+            
+        } else {
+            textLabel?.textColor = .lightGray
+            symbolLabel?.textColor = .lightGray
+        }
         return cell
     }
-    */
+    
+    private func getSymbolForTask(with status: TaskStatus) -> String {
+        var resultSymbol: String
+        if status == .planned {
+            resultSymbol = "\u{25CB}"
+        } else if status == .completed {
+            resultSymbol = "\u{25C9}"
+        } else {
+            resultSymbol = ""
+        }
+        return resultSymbol
+    }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title: String?
+        let taskType = sectionsTypesPosition[section]
+        if taskType == .impotant {
+            title = "Важные"
+        } else if taskType == .normal {
+            title = "Текущие"
+        }
+
+        return title
+    }
+
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
